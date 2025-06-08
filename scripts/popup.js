b2238244faf73b14d682d02f6c6656a541d8d49d
AlexSokol <astrasokol@yandex.ru>
2025-06-08 21:55:08 +0300

    Исправил имена переменных в popup.js

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,21 +1,14 @@
-const POPUP_OPENED_CLASSNAME = "popup_open";
-const BODY_FIXED_CLASSNAME = "body_fixed";
-const bodyNode = document.querySelector("body");
+const POPUP_OPENED_CLASSNAME_ = "popup_open";
+const BODY_FIXED_CLASSNAME_ = "body_fixed";
 
+const bodyNode_ = document.querySelector("body");
 const popupNode = document.querySelector(".js-popup");
-
 const btnOpenNode = document.querySelector(".js-btn");
-btnOpenNode.addEventListener("click", togglePopup);
-
-const btnCloseNode = document.querySelector(".js-popup__close-btn");
-btnCloseNode.addEventListener("click", togglePopup);
-
-function togglePopup() {
-  popupNode.classList.toggle(POPUP_OPENED_CLASSNAME);
-  bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
-}
-
 const popupContentNode = document.querySelector(".js-popup__content");
+const btnCloseNode = document.querySelector(".js-popup__close-btn");
+
+btnOpenNode.addEventListener("click", togglePopup);
+btnCloseNode.addEventListener("click", togglePopup);
 
 popupNode.addEventListener("click", (event) => {
   const isClickOutsideContent = !event
@@ -26,3 +19,8 @@ popupNode.addEventListener("click", (event) => {
     togglePopup();
   }
 });
+
+function togglePopup() {
+  popupNode.classList.toggle(POPUP_OPENED_CLASSNAME_);
+  bodyNode_.classList.toggle(BODY_FIXED_CLASSNAME_);
+}
